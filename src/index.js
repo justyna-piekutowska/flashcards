53453912cbbdfe9176e3352b80da2566c317ea15
Justyna Piekutowska <piekutowska.justyna@protonmail.com>
2019-06-04 19:28:31 +0200

    Flashcard shows random value in polish

--- src/index.js
+++ src/index.js
@@ -5,19 +5,56 @@ import './index.css';
 class App extends Component {
   render () {
     return (
-        <div className="header">
-         	<h1>YourOwnFlashcards</h1>
-         	<div className="hamburgerMenu">
-	    		<div className="hamburgerBars">
-	            	<div className="TopBar"></div>
-	            	<div className="CenterBar"></div>
-	            	<div className="BottomBar"></div>
-	         	</div> 
-         	</div>
+        <div>
+          <div className="header">
+           	<h1>YourOwnFlashcards</h1>
+           	<div className="hamburgerMenu">
+        		<div className="hamburgerBars">
+                	<div className="TopBar"></div>
+                	<div className="CenterBar"></div>
+                	<div className="BottomBar"></div>
+             	</div>
+           	</div>
+          </div>
+          <div className="flashcardElement">
+            <div className="flashcard">
+              <FlashcardText />
+            </div>
+          </div>
         </div>
       )
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+class FlashcardText extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {expression: {pl: String, en: String}};
+  }
 
+  componentDidMount() {
+    this.saveFlashcard();
+  }
+
+  getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  saveFlashcard() {
+    const flashcardsData = [{pl: "poprawny", en: "valid"}, {pl: "założyć", en: "assume"}, {pl: "zbiór/ustawić", en: "set"}];
+    const flashcardKey = this.getRandomInt(flashcardsData.length);
+    const selectedFlashcard = flashcardsData[flashcardKey];
+
+    this.setState({
+      expression: {pl: selectedFlashcard.pl, en: selectedFlashcard.en }
+    });
+  }
+
+  render() {
+    return (
+      <p id="flashcard_text">{this.state.expression.pl}</p>
+      );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
